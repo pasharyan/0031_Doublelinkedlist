906d82df7db542366e90b67cb47817e9fb437bf0
pasharyan <pasharyan50@gmail.com>
2026-04-30 15:48:12 +0700

    penyelesaian

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -232,3 +232,36 @@ int main()
         cout << "5. Search Record\n";
         cout << "6. Exit\n";
         cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "Invalid option\n";
+        }
+
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear"); // Gunakan system("cls") jika kamu menggunakan Windows
+    } while (choice != '6');
+}
